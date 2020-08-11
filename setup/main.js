d35496e845a8b718b2c8da7f0ebe1476d2b05e67
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-08-10 21:42:10 -0500

    added pants to the objects array, jumbotron, and buttons

--- setup/main.js
+++ setup/main.js
@@ -3,29 +3,161 @@
 
 // UNIVERSAL SCRIPTS
 const pantsObj = [{
-  image: 'https://img.alicdn.com/imgextra/i1/51768288/O1CN012B5x46hZVdJdhAO_!!51768288.jpg_640x640q90.jpg',
+  image: '/images/black-1.png',
   material: 'Wool',
   size: ['S', 'M', 'L'],
   age: 'Adult'
 },
 {
-  image: 'https://i.ebayimg.com/images/g/QwoAAOSw0ZJajkIn/s-l400.jpg',
+  image: '/images/silky-white.jpg',
   material: 'Silk',
   size: ['S', 'M', 'L'],
   age: 'Adult'
 },
 {
-  image: 'https://i.ebayimg.com/images/g/QwoAAOSw0ZJajkIn/s-l400.jpg',
+  image: '/images/teen-1',
   material: 'Cotton',
   size: ['S', 'M', 'L'],
   age: 'Teen'
   },
 {
-  image: 'https://i.ebayimg.com/00/s/ODAwWDgwMA==/z/r9QAAOSw29Zdup3B/$_12.JPG?set_id=880000500F',
+  image: '/images/teen-2.jpeg',
   material: 'Down',
   size: ['S', 'M', 'L'],
-  age: ['Adult', 'Teen', 'Children']
+  age: 'Teen'
   },
+{
+  image: '/images/kids-1.png',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Kid'
+  },
+{
+  image: '/images/camo-1.jpeg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/floral-1.png',
+  material: 'Silk',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/stars.jpeg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/kids-2.jpg',
+  material: 'Wool',
+  size: ['S', 'M', 'L'],
+  age: 'Kid'
+  },
+{
+  image: '/images/navy-1.png',
+  material: 'Down',
+  size: ['S', 'M', 'L'],
+  age: 'Teen'
+  },
+{
+  image: '/images/pirate-1.jpeg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/pattern.jpeg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/light-blue.jpeg',
+  material: 'Silk',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/tan-1.png',
+  material: 'Wool',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/kids-3.jpeg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Kid'
+  },
+{
+  image: '/images/camo-3.jpeg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/winter-1.jpeg',
+  material: 'Down',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/kids-4.jpg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Kid'
+  },
+{
+  image: '/images/floral-2.png',
+  material: 'Silk',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/floral-3.jpg',
+  material: 'Silk',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/kids-5.jpg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Kid'
+  },
+{
+  image: '/images/kids-6.jpg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Kid'
+  },
+{
+  image: '/images/camo-4.jpeg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/checkered-1.jpg',
+  material: 'Cotton',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/camo-5.jpeg',
+  material: 'Silk',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  },
+{
+  image: '/images/floral-4.jpeg',
+  material: 'Down',
+  size: ['S', 'M', 'L'],
+  age: 'Adult'
+  }
 ]
 // HOME SCRIPTS
 
