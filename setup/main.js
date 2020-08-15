90b6a9ca4f53bcec5e426feb0c1b22b75e3667d0
Ryan McNair <ryan.mcnair@gmail.com>
2020-08-15 09:37:50 -0500

    adding content for Contact Us page. Modified contact.html, main.css and main.js

--- setup/main.js
+++ setup/main.js
@@ -2,6 +2,7 @@
 
 
 // UNIVERSAL SCRIPTS
+
 const pantsObj = [{
   image: 'https://img.alicdn.com/imgextra/i1/51768288/O1CN012B5x46hZVdJdhAO_!!51768288.jpg_640x640q90.jpg',
   material: 'Wool',
@@ -33,35 +34,79 @@ const pantsObj = [{
 
 // CONTACT US SCRIPTS
 
-
+// EMPTY ARRAY TO STORE INPUT VALUE FROM NAME/EMAIL/RADIO BUTTON FORMS
 let contactFormInput = [];
 
-const contactSubmitButton = (e) => {
+// FUNCTION THAT PUTS HTML ON THE DOM ONCE SUBMIT BUTTON IS CLICKED
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+// FUNCTION THAT REMOVES HTML FROM THE DOM ONCE SUBMIT BUTTON IS CLICKED
+const removeFromDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+// FUNCTION THAT CAPTURES DATA FROM FORM INPUTS, STORES THEM IN AN OBJECT AND PUSHES THEM TO THE EMPTY ARRAY ABOVE
+const getNameEmail = (e) => {
   let buttonId = e.target.id;
-  if (buttonId === "submit-button") {
-    const name = document.querySelector("#nameInput").value;
-    const email = document.querySelector("#emailInput").value;
-    let contactFormObject = {username: name, useremail: email};
-    contactFormInput.push(contactFormObject);
+  if (buttonId === 'submit-button'){
+  const name = document.querySelector('#nameInput').value;
+  const email = document.querySelector('#emailInput').value;
+  const radioSelection = document.querySelector('input[name="exampleRadios"]:checked').value;
+  let contactFormObject = {name: name, email: email, bloomer: radioSelection};
+  contactFormInput.push(contactFormObject);
+  }
+}
+
+// FUNCTION THAT TRIGGERS getNameEmail AND thankYouMessage FUNCTIONS WHEN THE SUBMIT BUTTON IS CLICKED
+const submitButtonClick = () => {
+  if(document.getElementById('contactUsPage')){
+  let el = document.querySelector('#submit-button');
+  el.addEventListener("click", getNameEmail);
+  el.addEventListener("click", thankYouMessage);
   }
 };
 
-const contactErrorMessage = (name) => {
-  if (name) {
-    document.querySelector("#nameInput" && "#emailInput").innerHTML = ""
-  } else {
-    document.querySelector("#nameInput" && "#emailInput").innerHTML = 
-      `<div style="color: red; margin-bottom: 10px;">
-          <b>Enter your name and email, pretty please</b>
-      </div>`
-  }
-}
+// FUNCTION THAT TRIGGERS AN ALERT MESSAGE IF THE EMAIL INPUT VALUE IS EMPTY
+const emptyForm = () => {
+  let emailAddress = document.getElementById('emailInput').value;
+      if (emailAddress === ''){
+          alert("Please enter your email address");
+          return false; 
+      }
+      return true;
+};
 
-const contactSuccessMessage = (name) => {
-  if (name) {
-    document.querySelector()
+// FUNCTION THAT RUNS WHEN NAME AND EMAIL INPUT IS PRESENT. PRINTING HTML THAT INCLUDES DATA FROM THE ARRAY TO THE DOM. ALSO, REMOVES DOM HTML WHEN ENGAGED.
+const thankYouMessage = (e) => {
+  let buttonId = e.target.id
+  let domString = "";
+
+  if (buttonId === "submit-button")
+  for (let i = 0; i < contactFormInput.length; i++){
+    if (contactFormInput[i].name){
+      domString += `<div class="card text-center" id="thanks">
+                      <div class="card-header" id="thanks-header">
+                        Thank you!
+                      </div>
+                      <div class="card-body">
+                        <h5 class="card-title">${contactFormInput[i].name}, your interest in Bloomers is appreciated!</h5>
+                        <p class="card-text">We will send a message to ${contactFormInput[i].email} about ${contactFormInput[i].bloomer} bloomers within 24 hours.</p>
+                        <a href="index.html" class="btn btn-secondary">Home Page</a>
+                      </div>
+                      </div>`
+      removeFromDom('#page-content', "");
+    } else {
+      emptyForm();
+    }
   }
-}
+    printToDom('#contact-message', domString);
+};
+// CALLBACK FOR BUTTON CLICK FUNCTION
+submitButtonClick();
 
 // MOODS SCRIPTS
 
